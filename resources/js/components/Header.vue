11ffd2ff2087a9988bd8e5897a651f93d9577505
Chacha Nurholis <chachanurholis29@gmail.com>
2021-02-14 06:02:09 +0700

    Fix issues

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -108,51 +108,17 @@
                     </li>
                   </ul>
                 </li>
-                <!-- <li class="footer"><a href="#">See All Messages</a></li> -->
               </ul>
             </li>
-            <!-- <li class="dropdown notifications-menu">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="fa fa-bell-o"></i>
-                                <span class="label label-warning">10</span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li class="header">You have 10 notifications</li>
-                                <li>
-                                    <ul class="menu">
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="footer"><a href="#">View all</a></li>
-                            </ul>
-                        </li>
-            -->
             <li class="dropdown user user-menu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src class="user-image" alt="User Image" />
+                <img src="https://via.placeholder.com/160" class="user-image" alt="User Image" />
                 <span class="hidden-xs">{{ authenticated.name }}</span>
               </a>
               <ul class="dropdown-menu">
                 <li class="user-header">
                   <img src="https://via.placeholder.com/160" class="img-circle" alt="User Image" />
                   <p>{{ authenticated.name }}</p>
-                </li>
-                <li class="user-body">
-                  <div class="row">
-                    <!-- <div class="col-xs-4 text-center">
-                                            <a href="#">Followers</a>
-                                        </div>
-                                        <div class="col-xs-4 text-center">
-                                            <a href="#">Sales</a>
-                                        </div>
-                                        <div class="col-xs-4 text-center">
-                                            <a href="#">Friends</a>
-                    </div>-->
-                  </div>
                 </li>
                 <li class="user-footer">
                   <div class="pull-left">
